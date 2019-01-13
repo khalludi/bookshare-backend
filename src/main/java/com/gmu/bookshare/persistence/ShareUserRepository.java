779485ff0c536da1920f3dd1bd88cb0cb55a6f1b
Khalid Ali <khalludi123@gmail.com>
2019-01-12 23:50:18 -0500

    Add service code

--- src/main/java/com/gmu/bookshare/persistence/ShareUserRepository.java
+++ src/main/java/com/gmu/bookshare/persistence/ShareUserRepository.java
@@ -3,6 +3,9 @@ package com.gmu.bookshare.persistence;
 import com.gmu.bookshare.entity.ShareUser;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ShareUserRepository extends CrudRepository<ShareUser, Long> {
 
+    List<ShareUser> findByEmail(String s);
 }
--- src/main/java/com/gmu/bookshare/service/ShareUserService.java
+++ src/main/java/com/gmu/bookshare/service/ShareUserService.java
@@ -1,8 +1,11 @@
 package com.gmu.bookshare.service;
 
+import com.gmu.bookshare.entity.ShareUser;
 import com.gmu.bookshare.persistence.ShareUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ShareUserService {
@@ -12,5 +15,15 @@ public class ShareUserService {
     @Autowired
     public ShareUserService(ShareUserRepository shareUserRepository) {
         this.shareUserRepository = shareUserRepository;
+    }
+
+    public ShareUser addShareUser(ShareUser user) {
+        List<ShareUser> userList = shareUserRepository.findByEmail(user.getEmail());
+        if (userList.size() > 0) {
+            return userList.get(0);
+        } else {
+            shareUserRepository.save(user);
+            return user;
+        }
     }
 }
