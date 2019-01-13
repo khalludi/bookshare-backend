efd22e6a8755be208ab25484a7e9b1f331dc1ca8
Khalid Ali <khalludi123@gmail.com>
2019-01-12 23:38:39 -0500

    Create ShareUserDto

---
+++ src/main/java/com/gmu/bookshare/model/ShareUserDto.java
@@ -0,0 +1,22 @@
+package com.gmu.bookshare.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ShareUserDto {
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("name")
+    private String name;
+}
